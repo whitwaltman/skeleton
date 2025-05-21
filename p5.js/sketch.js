615433f0f92c20a86496e78fad4206b2c1704d42
Whit Waltman <c.lin@mailbox.org>
2025-05-20 17:47:08 -0700

    update p5 sketch skeleton

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -1,9 +1,24 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
+    // creates a square shape with a thin black border around the canvas
+    square(0, 0, 600); // x, y, size
+    // start with the mouse coordinates outside of the canvas so it doesn't render a circle on setup
+    mouseX = -300;
+    mouseY = -300;
 }
 
 function draw() {
-    background('magenta');
-    textSize(40);
-    text('Hello, world!', 40, 360);
+    // background('magenta');
+    // textSize(40);
+    // text('Hello, world!', 40, 360);
+
+    // when mouse button is pressed, circles turn black
+    if (mouseIsPressed === true) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+
+    // white circles drawn at mouse position
+    circle(mouseX, mouseY, 100);
 }
